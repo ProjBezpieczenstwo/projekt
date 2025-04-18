31a1b506e6602b429d4eda9e9f2876ee1641a85d
kuchaaar <kuba_kucharek@wp.pl>
2025-04-18 17:04:44 +0200

    config for azure

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     DB_USER = os.getenv('db_user')
     DB_PASSWORD = os.getenv('db_password')
     DB_URI = os.getenv('db_uri')
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_URI}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_URI}/{DB_NAME}?sslmode=require"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '6e48ff4302924803ba0df47c2e307c78'
     EMAIL_SERVICE_URL = os.getenv("email_service_uri")
