6e55e786afb18f283830b81d2e305b5f48243e0f
kuchaaar <kuba_kucharek@wp.pl>
2025-04-18 16:06:20 +0200

    config for azure

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+import os
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@postgresDB:5432/ariusDB'
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_URI = os.getenv('DB_URI')
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_URI}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '6e48ff4302924803ba0df47c2e307c78'
-    EMAIL_SERVICE_URL = "http://email_service:5001"
+    EMAIL_SERVICE_URL = os.getenv("EMAIL_SERVICE_URL")
     # JWT_SECRET_KEY = 'your-jwt-secret-key'
