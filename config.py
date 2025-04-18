d0fd3dbe9efd69f949784815d213086efe5ab758
kuchaaar <kuba_kucharek@wp.pl>
2025-04-18 16:10:00 +0200

    config for azure

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config:
-    DB_NAME = os.getenv('DB_NAME')
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_URI = os.getenv('DB_URI')
+    DB_NAME = os.getenv('db-name')
+    DB_USER = os.getenv('db-user')
+    DB_PASSWORD = os.getenv('db-password')
+    DB_URI = os.getenv('db-uri')
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_URI}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '6e48ff4302924803ba0df47c2e307c78'
-    EMAIL_SERVICE_URL = os.getenv("EMAIL_SERVICE_URL")
+    EMAIL_SERVICE_URL = os.getenv("email-service-uri")
     # JWT_SECRET_KEY = 'your-jwt-secret-key'
