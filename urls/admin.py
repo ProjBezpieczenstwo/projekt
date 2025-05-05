56e77f4bc2e0d1847e8f953378bba0ea9d0d43ac
kuchaaar <kuba_kucharek@wp.pl>
2025-05-05 21:46:15 +0200

    my bad

--- urls/admin.py
+++ urls/admin.py
@@ -90,9 +90,7 @@ def get_all_users():
 @admin.route('/users/<int:user_id>', methods=['DELETE'])
 @jwt_required(role='admin')
 def delete_user(user_id):
-    data = request.get_json()
-    current_app.logger.error(data)
-    user_type = data.get("user_type")
+    user_type = request.args.get("user_type")
     if user_type == "BaseUser":
         user = BaseUser.query.get(user_id)
     else:
