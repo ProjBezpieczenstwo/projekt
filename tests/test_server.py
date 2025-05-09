e8be53af6f8dcb5e38c230ae4fb4a3c5fd55828f
kuchaaar <kuba_kucharek@wp.pl>
2025-05-09 13:27:14 +0200

    test on fetch

--- tests/test_server.py
+++
@@ -1,4 +0,0 @@
-def test_404_error(test_client):
-    response = test_client.get('/non-existent-endpoint')
-    assert response.status_code == 404
-    assert response.json['error'] == 'Page not found'
